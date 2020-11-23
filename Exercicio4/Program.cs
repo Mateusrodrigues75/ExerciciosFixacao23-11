84e40ce34d3b3c33fc194c9f5dbc77e96d0fbf92
Mateus Rodrigues <m.rodriguessilva75@outlook.com>
2020-11-23 20:10:40 -0300

    Finalizado

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] valores = new float [10];
+            float menor = 0, maior = 0;
+            Console.WriteLine("------------Digite 10 valores---------------");
+            for (var i = 0; i < valores.Length; i++){
+                Console.Write($"Digite o {i+1}° valor: ");
+                valores[i] = float.Parse(Console.ReadLine());
+
+                menor=valores[0];
+                if(valores[i] < menor){
+                    
+                    menor = valores[i];
+                }
+                else if (valores[i] > maior){
+                    maior = valores[i];
+                }
+            }
+            Console.WriteLine("O menor número é:" + menor);
+            Console.WriteLine("O maior número é:" + maior);
+        }
+    }
+}
