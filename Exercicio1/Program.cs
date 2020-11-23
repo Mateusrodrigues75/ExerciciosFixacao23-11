b0dc2fb3d69de207ef84b2252facab418c295b0b
Mateus Rodrigues <m.rodriguessilva75@outlook.com>
2020-11-23 11:13:59 -0300

    Finalizado

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoAtual, anoNascimento, idade;
+            Console.WriteLine("Validação de Idade para Votação nas Eleições");
+            Console.Write("Digite o Ano Atual: ");
+            anoAtual = int.Parse(Console.ReadLine());
+            
+            Console.Write("Digite o Ano do seu nascimento: ");
+            anoNascimento = int.Parse(Console.ReadLine());
+
+            idade = anoAtual - anoNascimento;
+
+            if(idade >= 16){
+                Console.WriteLine("Você poderá votar este ano.");
+            }else{
+                Console.WriteLine("Você não poderá votar este ano.");
+                
+            }
+
+
+
+            
+        }
+    }
+}
