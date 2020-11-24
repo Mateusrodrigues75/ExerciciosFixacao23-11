3765fcb25eb133043e3f88e8dde58cf4c6dd10a7
Mateus Rodrigues <m.rodriguessilva75@outlook.com>
2020-11-23 23:26:24 -0300

    Finalizado

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplicacao;
+            int[] limite = new int [10];
+            Console.WriteLine("Tabuada de 1 a 10");
+            Console.WriteLine("-----------------");
+
+            for (var i = 1; i < 11; i++)
+            {
+                Console.WriteLine($"Tabuada do {i}");
+
+                for (var cont = 1; cont < 11; cont++)
+                {
+                    multiplicacao = i * cont;
+                    Console.WriteLine ($"{i} * {cont} = {multiplicacao}");
+                }
+                Console.WriteLine("-----------------------------");
+
+            }
+
+        }
+    }
+}
