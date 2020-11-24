2425308a68c698532fb0481fcc568cfce7ede3cd
Mateus Rodrigues <m.rodriguessilva75@outlook.com>
2020-11-24 09:14:23 -0300

    Finalizado

---
+++ Exercicio7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           float[] valores = new float[15];
+            for (var i = 0; i < 15; i++) 
+            {
+
+            Console.WriteLine("Digite o número: ");
+            valores[i] = float.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Lista dos números na ordem inversa: ");
+
+            Array.Reverse(valores);
+            
+            foreach(float lista in valores){
+                Console.WriteLine(lista);
+                
+            }
+            
+        }
+    }
+}
